a79bcee45304950d58a6499344185c3710c79cf3
vishnuprabha72 <vishnuprabha72@gmail.com>
2026-03-13 13:26:59 +0530

    updated target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://rhetorical-role-labelling.onrender.com",
         changeOrigin: true,
       },
     },
